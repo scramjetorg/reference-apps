5116579485490c50e6f39ca2c0fe106ce04489c7
Michal Piotrowski <michal.piotrowski@scramjet.org>
2022-07-01 10:51:37 +0200

    Modyfing stop samples

--- python/can-keep-alive/main.py
+++ python/can-keep-alive/main.py
@@ -4,7 +4,7 @@ import inspect
 async def run(context, input, *args):
     if args[0] == 'SEND_KEEPALIVE':
         context.set_stop_handler(
-            context.keep_alive(args[1])
+            lambda timeout = 0, can_keep_alive = False: context.keep_alive(args[1])
         )
 
     await asyncio.sleep(10**9)
--- python/stop-handler/stop-handler.py
+++ python/stop-handler/stop-handler.py
@@ -6,5 +6,7 @@ async def stop_handler():
     print("Cleanup done.")
 
 async def run(context, input):
-    context.set_stop_handler(stop_handler)
+    context.set_stop_handler(
+        lambda timeout = 0, can_keep_alive = False: stop_handler()
+    )
     await asyncio.sleep(10**9)
