162b0b06e68e80846c82595f9cff44fdc052aabf
Piotr <ypiotrek.falba@gmail.com>
2023-04-14 11:03:57 +0000

    Changed the way how data is submited to topic

--- js/self-starting-sequence/index.ts
+++ js/self-starting-sequence/index.ts
@@ -1,6 +1,10 @@
 import { ReadableApp } from "@scramjet/types";
 import { defer } from "@scramjet/utility";
+import { PassThrough } from "stream";
 
+const output: PassThrough & { topic: string, contentType: string } = Object.assign(
+    new PassThrough({ encoding:"utf-8" }), { topic: "self-starting-data", contentType: "text/plain" }
+);
 const app: ReadableApp<string> = async function(_stream,interval:number) {
     const hub = this.hub!;
     const instanceId = this.instanceId;
@@ -10,11 +14,11 @@ const app: ReadableApp<string> = async function(_stream,interval:number) {
 
     await defer(interval);
     this.logger.info(`starting sequence with ${interval} ms interval `);
-    await hub.sendNamedData("info", `starting with id: ${instanceId} +/n`);
+    output.push(`starting with id: ${instanceId} +/n`);
     await seqClient.start({ appConfig:{} });
     await instanceClient.kill({ removeImmediately:true });
 
-    return Promise.resolve("end");
+    return output
 };
 
 export default app;
