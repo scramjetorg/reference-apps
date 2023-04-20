619a675b8265edbd09ab680c0e5a136fc44e7e10
Piotr <53794300+piotrek6641@users.noreply.github.com>
2023-04-20 09:27:18 +0100

    Clickup add issues (#21)
    Implemented new sequence which can send data to clickup api
    ---------
    
    Co-authored-by: Piotr <ypiotrek.falba@gmail.com>
    Co-authored-by: Patryk Falba <patuwwy@gmail.com>

---
+++ js/clickup-add-issues/README.md
@@ -0,0 +1,21 @@
+# Clickup add issues
+
+This sequence reads info send by [git-read-issues]() then adds all the issues to a clickup list.
+
+> ! This sequence should be used only in combination with git-read-issues
+
+## Setup
+
+Change data in `cudata.json` to one that applies to you
+
+```json
+{
+    "listId":"list-id",//id of the list where issues will be placed
+}
+```
+
+When running this sequence it is necessary to use clickup api key as an argument
+
+## Git-read-issues
+
+[click here to see this sequence](https://github.com/scramjetorg/reference-apps/tree/main/js/git-read-issues)
---
+++ js/clickup-add-issues/clickupClient.ts
@@ -0,0 +1,52 @@
+/* eslint-disable no-console */
+import { request } from "https";
+import { ClientRequest } from "http";
+import * as cuSettings from "./cudata.json";
+
+type CuRequestType = {
+    name: string;
+    description: string;
+    tags: Array<string>;
+}
+
+export class ClickupClient {
+    listId: string;
+    token: string;
+
+    constructor(token:string) {
+        this.listId = cuSettings.listId;
+        this.token = token;
+        console.log(token);
+    }
+    sendRequest(issue :CuRequestType) {
+        const body = JSON.stringify({
+            name: issue.name,
+            description: issue.description,
+            tags:issue.tags
+        });
+        const options = {
+            hostname: "api.clickup.com",
+            path: `/api/v2/list/${this.listId}/task`,
+            port: 443,
+            headers: {
+                Authorization: this.token,
+                "content-type": "application/json"
+            },
+            method: "POST",
+        };
+        const req: ClientRequest = request(options, (res) => {
+            console.log("statusCode:", res.statusCode);
+            console.log("headers:", res.headers);
+
+            res.on("data", (d) => {
+                process.stdout.write(d);
+            });
+        });
+
+        req.on("error", (e) => {
+            console.error(e);
+        });
+        req.write(body);
+        req.end();
+    }
+}
