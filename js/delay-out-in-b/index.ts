634d1eeef4bd76c66a9f791c3d051c4bf0e58bae
a-tylenda <agnieszka.tylenda@scramjet.com>
2023-06-27 10:33:06 +0000

    Add comments to delay sequences

--- js/delay-out-in-b/index.ts
+++ js/delay-out-in-b/index.ts
@@ -1,30 +1,31 @@
-/* eslint-disable no-console */
-/* eslint-disable no-loop-func */
 import { AppConfig, AppContext, Streamable } from "@scramjet/types";
-
 import { StringStream } from "scramjet";
 
 const rht = require("./real-hrtime.node");
 
 const CUTOFF = 2000;
-
 const diffs: bigint[] = [];
 let minDiff = BigInt(Number.MAX_SAFE_INTEGER);
 let maxDiff = BigInt(Number.MIN_SAFE_INTEGER);
 
 const bigIntMax = (...args: bigint[]) => args.reduce((m, e) => e > m ? e : m);
 const bigIntMin = (...args: bigint[]) => args.reduce((m, e) => e < m ? e : m);
-//
+
+/**
+ * This Sequence calculates the time differences between received timestamps and the local timestamp.
+ * It performs statistical analysis on the differences and returns the results as a JSON string.
+ *
+ * @param {AppContext} this - Application context
+ * @param {Streamable} input - Input stream containing the timestamps to analyze
+ * @param {number} timesOfExecution - Number of times the analysis should be performed (default: 12000)
+ * @returns {Promise<string>} - Promise that resolves to a JSON string containing the analysis results
+ */
 const exp: [
     { requires: string, contentType: string},
     (this: AppContext<AppConfig, any>, input: Streamable<any>) => Promise<string>
 ] = [
     { requires: "delay-test", contentType: "application/x-ndjson" },
-    /**
-     *
-     * @param input - internal stream
-     * @returns - transformed stream
-     */
+
     async function(input: Streamable<any>, timesOfExecution = 12000) {
         let h: bigint = BigInt(0);
         let diff = BigInt(0);
@@ -32,8 +33,8 @@ const exp: [
 
         await (input as StringStream)
             .while(o => {
-                h = rht.bigint();
-                diff = h - BigInt(o);
+                h = rht.bigint();  // Get the current local timestamp
+                diff = h - BigInt(o);  // Calculate the difference between local timestamp and received timestamp
 
                 if (diff < 0) {
                     this.logger.warn(`Package has been teleported in time! (${h.toString()} is less than ${BigInt(o.ts).toString()} at entry ${o.i}: ${o}) or machines time mismatch.`);
@@ -48,9 +49,11 @@ const exp: [
 
         this.logger.trace("Instance finished, total entries:", diffs.length);
 
-        diffs.splice(0, CUTOFF);
+        diffs.splice(0, CUTOFF);  // Remove the initial cutoff entries from the differences array
 
-        const sum = diffs
+        // calculate the sum of all differences in the diffs array
+        // update the minimum and maximum differences.
+        const sum: bigint = diffs
             .reduce(
                 (a, b) => {
                     minDiff = bigIntMin(minDiff, b);
@@ -62,10 +65,10 @@ const exp: [
 
         return JSON.stringify({
             units: "ns",
-            avg: parseInt((sum / BigInt(diffs.length)).toString(), 10),
-            total: diffs.length,
-            max: parseInt(maxDiff.toString(), 10),
-            min: parseInt(minDiff.toString(), 10)
+            avg: parseInt(((sum / BigInt(diffs.length))).toString(), 10),  // Calculate the average difference
+            total: diffs.length,  // Total number of differences
+            max: parseInt(maxDiff.toString(), 10),  // Maximum difference
+            min: parseInt(minDiff.toString(), 10)  // Minimum difference
         });
     }
 ];
